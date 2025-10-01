3016ab1a1d85deac35a3a320c0d9314306520484
ortigosa1971 <eliasalfarnate@gmail.com>
2025-10-01 07:34:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js — CommonJS
+erver.js — CommonJS
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -11,16 +11,14 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-// Basic route
-app.get("/", (req, res) => {
-  res.send("OK");
-});
+// Health check
+app.get("/", (req, res) => res.send("OK"));
 
+// Start
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor en http://localhost:${PORT}`);
 });
-
 
 
 
